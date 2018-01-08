314a4f42953a860b6bd467e92c6bef5a46fc9f5b
Diego <d_bernalgomez@fanshaweonline.ca>
2018-01-07 21:18:30 -0500

    Git modified

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,20 @@
 module.exports = function(grunt){
 
     grunt.initConfig({
-
+      sass: {
+        options: {
+          outputStyle : 'expanded'
+        },
+        dist: {
+          files: [{
+            expand : true,
+            cwd: 'sass/',
+            src: '*.scss',
+            dest: 'css/'
+            ext: '.css'
+          }]
+        }
+      }
     });
 
     grunt.loadNpmTasks('grunt-sass');
