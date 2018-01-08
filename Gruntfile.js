f160e4dcac51e36bacb1a9eadd7d727ead3e409b
Shan Jiang <shanjiang@Shan-Jiang.local>
2018-01-08 11:25:51 +0800

    fixed grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,27 @@
-module.exports = function(grunt){
-
+module.exports = function (grunt) {
     grunt.initConfig({
-      sass: {
-        options: {
-          outputStyle : 'expanded'
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css/styles.css': 'sass/styles.scss'
+                }
+            }
         },
-        dist: {
-          files: [{
-            expand : true,
-            cwd: 'sass/',
-            src: '*.scss',
-            dest: 'css/'
-            ext: '.css'
-          }]
+
+        watch: {
+            sass: {
+                files: ['sass/styles.scss', 'sass/*.scss'],
+                tasks: ['sass']
+            }
         }
-      }
     });
 
-    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('default', ['sass', 'watch']);
 };
--- js/video.js
+++ js/video.js
@@ -1,16 +1,14 @@
 (function () {
-    var video = document.querySelectorAll('.demo_reel');
+    var video = document.querySelector('.demo_reel');
 
     function videoPlay() {
+        console.log('sss');
         if (this.paused) {
             this.play();
-            document.querySelectorAll('.demo_reel').setAttribute("style","opacity:1; -moz-opacity:1; filter:alpha(opacity=100)");
         } else {
             this.pause();
         }
     }
 
-    video.forEach(function (videoItem) {
-        videoItem.addEventListener('click', videoPlay, false);
-    });
+    video.addEventListener('click', videoPlay, false);
 })();
